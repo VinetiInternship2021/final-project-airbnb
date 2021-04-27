0cf9b64bda75072ea41371521b6b61a14d6abbb4
Arthur <hayart98@gmail.com>
2021-04-27 11:36:35 +0400

    slider in boostrap card working great using npm Awesom-slider

--- frontend/src/components/slider/Slider.js
+++ frontend/src/components/slider/Slider.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import Carousel from 'react-bootstrap/Carousel'
+import AwesomeSlider from 'react-awesome-slider'
+import withAutoplay from 'react-awesome-slider/dist/autoplay'
+import 'react-awesome-slider/dist/styles.css'
 
-export default function Slider() {
+const AutoplaySlider = withAutoplay(AwesomeSlider)
+
+export default function Slider({ imgList }) {
     return (
-        <div>
-            adasdasdasd
-            <div>asdasd</div>
-        </div>
+        <AutoplaySlider
+            play={true}
+            cancelOnInteraction={true} // should stop playing on user interaction
+            interval={1000}
+        >
+            {imgList.map(({ src }, index) => (
+                <div data-src={src} key={index + src} />
+            ))}
+        </AutoplaySlider>
     )
 }
