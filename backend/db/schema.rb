2493d80bec312c9136f7cdf2c8cfcd3bb35746b2
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2021-05-02 00:46:12 +0400

    change some field types from str to in, add field 'currency'

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,21 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_122747) do
+ActiveRecord::Schema.define(version: 2021_05_01_181653) do
 
-  create_table "properties", force: :cascade do |t|
-    t.string "title"
-    t.string "propType"
+  create_table "apartments", force: :cascade do |t|
+    t.string "name"
+    t.string "apartmentType"
     t.string "address"
     t.string "price"
     t.string "beds"
     t.string "rooms"
     t.string "guests"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_apartments_on_user_id"
+  end
+
+  create_table "hosts", force: :cascade do |t|
+    t.string "firstName"
+    t.string "lastName"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "title"
+    t.string "propType"
+    t.string "address"
+    t.integer "price"
+    t.integer "beds"
+    t.integer "rooms"
+    t.integer "guests"
     t.text "description"
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "currency"
     t.index ["user_id"], name: "index_properties_on_user_id"
+  end
+
+  create_table "reg_users", force: :cascade do |t|
+    t.string "firstName"
+    t.string "lastName"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,5 +71,6 @@ ActiveRecord::Schema.define(version: 2021_04_30_122747) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "apartments", "users"
   add_foreign_key "properties", "users"
 end
