9d2f455726d820b2903a694aefdcf21d26134b78
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2021-05-02 00:43:57 +0400

    change some field types from str to in, add field 'currency'

---
+++ backend/db/migrate/20210501181653_change_several_types_in_property.rb
@@ -0,0 +1,9 @@
+class ChangeSeveralTypesInProperty < ActiveRecord::Migration[6.1]
+  def change
+    change_column :properties, :beds, :integer
+    change_column :properties, :rooms, :integer
+    change_column :properties, :guests, :integer
+    change_column :properties, :price, :integer
+    add_column :properties, :currency, :string
+  end
+end
