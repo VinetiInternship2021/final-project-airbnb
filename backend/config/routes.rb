aaaf7868d0226fbf65986990dc8e9b215813985a
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2021-05-02 01:07:30 +0400

    validations for user

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resource :users, only: [:index]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
+  post "/users", to: "users#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
