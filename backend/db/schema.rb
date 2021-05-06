db8a6b1e7759fabd1972cc5c741063c2136462c4
Arthur <hayart98@gmail.com>
2021-05-06 10:58:23 +0400

    Mini bug fix

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_063502) do
-
-  create_table "hosts", force: :cascade do |t|
-    t.string "firstName"
-    t.string "lastName"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_05_06_065631) do
 
   create_table "img_lists", force: :cascade do |t|
     t.string "imgUrl"
@@ -44,15 +35,6 @@ ActiveRecord::Schema.define(version: 2021_05_06_063502) do
     t.string "latitude"
     t.string "longitude"
     t.index ["user_id"], name: "index_properties_on_user_id"
-  end
-
-  create_table "reg_users", force: :cascade do |t|
-    t.string "firstName"
-    t.string "lastName"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
