aae55b92e43001bfb14ec3723cb036391ad6075a
Arthur <hayart98@gmail.com>
2021-05-08 19:35:17 +0400

    Changes headers bt for easy navigate

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -27,4 +27,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps, null)(Header)
--- frontend/src/components/header/headerBtn/HeadersBtn.js
+++ frontend/src/components/header/headerBtn/HeadersBtn.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { createUser } from '../../redux/actions'
@@ -6,7 +6,7 @@ import { createUser } from '../../redux/actions'
 import { useHistory } from 'react-router-dom'
 import { success } from '../../../notification/notiication'
 
-const HeadersBtn = ({ isLoggedIn, role, createUser }) => {
+const HeadersBtn = ({ currentUser, createUser }) => {
     const redirect = useHistory()
     function logOut() {
         createUser()
@@ -14,68 +14,87 @@ const HeadersBtn = ({ isLoggedIn, role, createUser }) => {
         success('Log out')
     }
 
-    return (
-        <>
-            {!isLoggedIn.length || !isLoggedIn[0] ? (
-                <>
-                    <Link
-                        className="btn btn-success reg"
-                        type="submit"
-                        to="/sign_in"
-                    >
-                        Sign in
-                    </Link>
-                    <Link
-                        className="btn btn-warning reg"
-                        type="submit"
-                        to="/sign_up"
-                    >
-                        Sign up
-                    </Link>
-                </>
-            ) : role === 'host' ? (
-                <>
-                    <Link
-                        className="btn btn-success reg"
-                        type="submit"
-                        to="/sign_in"
-                    >
-                        Add new
-                    </Link>
-                    <Link
-                        className="btn btn-success reg"
-                        to="/"
-                        onClick={logOut}
-                    >
-                        Log out
-                    </Link>
-                </>
-            ) : (
-                <>
-                    <Link
-                        className="btn btn-success reg"
-                        type="submit"
-                        to="/sign_in"
-                    >
-                        My Apartment
-                    </Link>
-                    <Link
-                        className="btn btn-success reg"
-                        to="/"
-                        onClick={logOut}
-                    >
-                        Log out
-                    </Link>
-                </>
-            )}
-        </>
-    )
+    useEffect(() => {
+        console.log()
+    }, [])
+    // return <button onClick={logOut}>asdasd</button>
+    if (!currentUser.status.length || !currentUser.status[0]) {
+        return (
+            <>
+                <Link
+                    className="btn btn-success reg"
+                    type="submit"
+                    to="/sign_in"
+                >
+                    Sign in
+                </Link>
+                <Link
+                    className="btn btn-warning reg"
+                    type="submit"
+                    to="/sign_up"
+                >
+                    Sign up
+                </Link>
+            </>
+        )
+    }
+    if (currentUser.status[0].user.role === 'host') {
+        return (
+            <>
+                <Link
+                    className="btn btn-success reg"
+                    type="submit"
+                    to="/add_new"
+                >
+                    Add new
+                </Link>
+                <Link
+                    className="btn btn-success reg"
+                    type="submit"
+                    to="/sign_in"
+                >
+                    My Apartments
+                </Link>
+                <Link className="btn btn-success reg" to="/" onClick={logOut}>
+                    Log out
+                </Link>
+            </>
+        )
+    }
+
+    if (currentUser.status[0].user.role === 'reg') {
+        return (
+            <>
+                <Link
+                    className="btn btn-success reg"
+                    type="submit"
+                    to="/sign_in"
+                >
+                    My Orders
+                </Link>
+                <Link className="btn btn-success reg" to="/" onClick={logOut}>
+                    Log out
+                </Link>
+            </>
+        )
+    }
+    if (currentUser.status[0].user.role === 'admin') {
+        return (
+            <>
+                <Link className="btn btn-success reg" to="/users">
+                    Users
+                </Link>
+                <Link className="btn btn-success reg" to="/" onClick={logOut}>
+                    Log out
+                </Link>
+            </>
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
     return {
-        isLoggedIn: state.user.currentUser.status,
-        role: state.user.currentUser?.status[0]?.role,
+        currentUser: state.user?.currentUser,
     }
 }
 const mapDispatchToProps = {
