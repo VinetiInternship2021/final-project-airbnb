0d7fe116f3a7cb874b33c1feccf3dd60d06d5d3f
Marieta Harutyunyan <harutyunyanmarieta@gmail.com>
2021-05-10 02:27:48 +0400

    rubycon on validations

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :firstName, presence: true, length: { minimum: 2, maximum: 128 }
-    validates :lastName, presence: true, length: { minimum: 2, maximum: 128 }
-    validates :email, presence: true,
-    format: { with: /\A(.+)@(.+)\z/ },
-    uniqueness: { case_sensitive: true },
-    length: { minimum: 4, maximum: 254 }
-    validates :role, presence: true, inclusion: { in: %w(host regular) }
-    
-    has_many :property
-    has_secure_password
+  validates :email, presence: true,
+                    format: { with: /\A(.+)@(.+)\z/ },
+                    uniqueness: { case_sensitive: true },
+                    length: { minimum: 4, maximum: 254 }
+  validates :firstName, presence: true, length: { minimum: 2, maximum: 128 }
+  validates :lastName, presence: true, length: { minimum: 2, maximum: 128 }
+  validates :role, presence: true, inclusion: { in: %w[host regular] }
+
+  has_many :property
+  has_secure_password
 end
