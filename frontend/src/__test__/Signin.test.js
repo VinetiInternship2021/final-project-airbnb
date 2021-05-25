fd1f47ff755182d824c197ef3ef9c8e6b4c41a45
Arthur <hayart98@gmail.com>
2021-05-25 20:31:22 +0400

    Fix test run bug and mini clean code

--- frontend/src/__test__/Signin.test.js
+++ frontend/src/__test__/Signin.test.js
@@ -6,12 +6,12 @@ import enzymeConfig from './enzymeConfig'
 describe('User forms', () => {
     it('Should sign in', () => {
         const component = mount(<SignInTest />)
-        const email = component.find('[data-testid="email"]').at(0)
+        const email = component.find('[data-testid="email"]')
+        
         email.instance().value = 'hi@hotmail.com'
         email.simulate('change')
         expect(
-            component.find('[data-testid="email"]').at(0).find('input').get(0)
-                .props.value
+            component.find('[data-testid="email"]').get(0).props.value
         ).toEqual('hi@hotmail.com')
     })
 })
