efd5f95594ec93761ad149dd2bb1561fcfb2323c
Arthur <hayart98@gmail.com>
2021-05-25 11:40:52 +0400

    User roter test

--- backend/spec/routing/users_routing_spec.rb
+++ backend/spec/routing/users_routing_spec.rb
@@ -1,30 +1,26 @@
+#rspec ./spec/routing/users_routing_spec.rb
 require "rails_helper"
-
 RSpec.describe UsersController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/users").to route_to("users#index")
+  describe "routing" do    
+
+    it "routes to userLists" do
+      expect(get: "/userLists").to route_to("users#index")
+    end
+    it "logged user" do
+      expect(get: "/auto_login").to route_to("users#auto_login")
     end
 
-    it "routes to #show" do
-      expect(get: "/users/1").to route_to("users#show", id: "1")
+    it "sing in" do
+      expect(put: "/login").to route_to("users#login")
     end
 
-
-    it "routes to #create" do
+    it "resource :users #create" do
       expect(post: "/users").to route_to("users#create")
-    end
+    end 
 
-    it "routes to #update via PUT" do
-      expect(put: "/users/1").to route_to("users#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/users/1").to route_to("users#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/users/1").to route_to("users#destroy", id: "1")
-    end
+    it "Is Login #auto_login" do
+      expect(get: "/auto_login").to route_to("users#auto_login")
+    end  
+  
   end
 end
