d34a02468e83f6d55a0eb4a6bc827379dd28a05d
Arthur <hayart98@gmail.com>
2021-05-25 10:12:04 +0400

    First success test ;)

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    user = User.new(firstName: "Arthur",
+      lastName: "Harutyunyan",
+      email:"Arthurart@hotmail.com",
+      password: "123456",
+      role: "host"
+      )
+    expect(user).to be_valid
+  end
 end
