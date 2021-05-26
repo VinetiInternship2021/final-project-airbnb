29e469341ad528902682b8ab7c10b76b0ec87642
Arthur <hayart98@gmail.com>
2021-05-26 12:07:57 +0400

    default inactive

--- backend/db/migrate/20210505222515_set_default_active_value_to_user.rb
+++ backend/db/migrate/20210505222515_set_default_active_value_to_user.rb
@@ -1,5 +1,5 @@
 class SetDefaultActiveValueToUser < ActiveRecord::Migration[6.1]
   def change
-    change_column_default(:users, :isActive, true)
+    change_column_default(:users, :isActive, false)
   end
 end
